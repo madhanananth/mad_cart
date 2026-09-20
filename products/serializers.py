e77abaec42a4bde3d70e9b2f94262931fd588022
Madhan <mathanmathan98234@gmail.com>
2026-09-20 13:36:25 +0530

    tiny error solved

--- products/serializers.py
+++ products/serializers.py
@@ -5,7 +5,7 @@ from .models import Category, Product
 class CategorySerializer(serializers.ModelSerializer):
      class Meta:
           model = Category
-          fields = ['id', 'name', 'description','image', 'featured']
+          fields = ['id', 'name', 'description','image', 'feature']
 
 class ProductSerializer(serializers.ModelSerializer):
      category_detail = CategorySerializer(source='category', read_only=True)
